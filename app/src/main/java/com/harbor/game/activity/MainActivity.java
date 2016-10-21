86aac3ffbfe6c7a6d4044b518729a8ae231e6443
harbor <hluo@bdna.com>
2016-10-21 11:23:59 +0800

    add i18n resource value

--- app/src/main/java/com/harbor/game/activity/MainActivity.java
+++ app/src/main/java/com/harbor/game/activity/MainActivity.java
@@ -118,10 +118,10 @@ public class MainActivity extends AbstractActivity implements View.OnClickListen
         }
 
         this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, getResources().getString(R.string.game_text_label_back_prompt), Toast.LENGTH_SHORT).show();
 
         new Handler().postDelayed(new Runnable() {
-
             @Override
             public void run() {
                 doubleBackToExitPressedOnce=false;
